1db805dca9de9c233bfa971579f9e5d4b089c7cb
Italo-Maia <italomaiatech@gmail.com>
2023-02-11 20:15:59 -0300

    feat(src): Adiciona método JSON do express

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+// app.use((request, response) => {
+
+// });
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000);
