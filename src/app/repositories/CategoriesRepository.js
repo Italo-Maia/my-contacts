7aca0f09b5a56db390bd266dd0efe61aa7ba5f24
Italo-Maia <italomaiatech@gmail.com>
2023-02-14 21:20:54 -0300

    feat(Main): Adiciona controller de rotas

---
+++ src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,23 @@
+const db = require("../../database");
+
+// MUST be only access data font
+class CategoriesRepository {
+    async findAll(orderBy) {
+
+    }
+
+    async create({ name }) {
+        console.log(name)
+
+        const [row] = await db.query(`
+            INSERT INTO categories(name)
+            VALUES($1)
+            RETURNING *
+        `, [name]);
+
+        return row;
+    }
+
+}
+
+module.exports = new CategoriesRepository();
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoyController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
@@ -9,6 +10,10 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts/', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+
+router.get('/categories', CategoyController.index);
+router.post('/categories', CategoyController.store);
 
 
 module.exports = router;
