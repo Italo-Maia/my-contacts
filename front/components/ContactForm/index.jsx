a4fd7a0490327070eb68bfe46717a20e035b7e0a
italo <italomaiatech@gmail.com>
2023-03-21 19:18:39 -0400

    refactor(ContactForm): Adiciona validações

--- front/components/ContactForm/index.jsx
+++ front/components/ContactForm/index.jsx
@@ -1,33 +1,99 @@
+import { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import FormGroup from '../FormGroup';
 import { ButtonContainer, Form } from './styles';
+
+import isEmailValid from '@/utils/isEmailValid';
 
 import Input from '../stylizedComponents/Input';
 import Select from '../stylizedComponents/Select';
 import Button from '../stylizedComponents/Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+
+    if (e.target.value && !isEmailValid(e.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail inválido' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'email',
+      ));
+    }
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+
+    if (e.target.value.length < 3) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome deve conter no mínimo 3 letras' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          value={name}
+          error={getErrorMessageByFieldName('name')}
+          onChange={handleNameChange}
+          placeholder="Nome"
+        />
       </FormGroup>
 
-      <FormGroup
-        error="Formato inválido"
-      >
+      <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
-          error
+          value={email}
+          error={getErrorMessageByFieldName('email')}
+          onChange={handleEmailChange}
           placeholder="E-mail"
         />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Telefone"
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
           <option value="instagram">Instagram</option>
         </Select>
 
@@ -40,6 +106,7 @@ export default function ContactForm({ buttonLabel }) {
     </Form>
   );
 }
+
 ContactForm.PropTypes = {
   buttonLabel: PropTypes.string.isRequired,
 };
