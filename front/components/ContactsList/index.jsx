b5cf50348d80b47b5f90fa5ce7b99f80ea543fca
italo <italomaiatech@gmail.com>
2023-03-24 15:33:16 -0400

    feat(front): Adiciona useMemo

--- front/components/ContactsList/index.jsx
+++ front/components/ContactsList/index.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import {
   Container, Header, Card, ListHeader,
 } from './styles';
@@ -8,12 +8,18 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import { InputSearchContainer } from '../Header/styles';
 // import Modal from '../Modal';
 // import Loader from '../Loader';
 
 export default function ContactsList() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = useMemo(() => contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )), [contacts, searchTerm]);
 
   useEffect(() => {
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
@@ -30,19 +36,33 @@ export default function ContactsList() {
     );
   }
 
+  function handleChangeSearchTerm(e) {
+    setSearchTerm(e.target.value);
+  }
+
   return (
     <Container>
       {/* <Loader /> */}
       {/* <Modal danger title="MODAL" text="TEXT" actionTitle="DELETAR" /> */}
 
+      <InputSearchContainer>
+        <input
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+          type="text"
+          placeholder="Pesquise pelo nome"
+        />
+      </InputSearchContainer>
+
       <Header>
         <strong>
-          {contacts.length}
-          {contacts.length === 1 ? ' contato' : ' contatos'}
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
         </strong>
         <Link href="/new">Novo Contato</Link>
       </Header>
 
+      {filteredContacts.length > 1 && (
       <ListHeader>
         <button
           type="button"
@@ -56,8 +76,9 @@ export default function ContactsList() {
           />
         </button>
       </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
--- front/components/Header/index.jsx
+++ front/components/Header/index.jsx
@@ -1,18 +1,12 @@
 import Image from 'next/image';
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 import logo from '@/assets/images/logo.svg';
 
-export default function Header({ withSearch }) {
+export default function Header() {
   return (
     <Container>
       <Image priority src={logo} alt="Imagem Mycontacts" width="201" />
-      { withSearch
-        && (
-        <InputSearchContainer>
-          <input type="text" placeholder="Pesquise pelo nome" />
-        </InputSearchContainer>
-        )}
     </Container>
   );
 }
